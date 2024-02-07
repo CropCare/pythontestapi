d001f7cecff32396fcd44ec3edd782ccd6ac874f
mtechson <ahmedson3001@gmail.com>
2024-02-08 01:32:35 +0200

    changing from local to server on mysql

--- api/index.py
+++ api/index.py
@@ -12,7 +12,7 @@ import bcrypt
 # initialization
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/backend'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://sql8682485:HAGEakjp9M@sql8.freemysqlhosting.net:3306/sql8682485'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # extensions
